7f942f481032fd881b819f42a80ec4dd6496efb2
Sylwia Litynska <sylwia.litynska@gmail.com>
2023-02-28 20:28:46 +0100

    5.5

--- js/script.js
+++ js/script.js
@@ -2,6 +2,53 @@ let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
+function getMoveName(argMoveId){
+  if(argMoveId == 1){
+    return 'kamień';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+
+  if(argMoveId == 2){
+    return 'papier';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+
+  if(argMoveId == 3){
+    return 'nożyce';
+  } else {
+    printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+    return 'nieznany ruch';
+  }
+}
+
+function displayResult(argComputerMove, argPlayerMove){
+
+  console.log('moves:', argComputerMove, argPlayerMove);
+  printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
+
+  if( argComputerMove == 'kamień' && argPlayerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+  } else {
+    printMessage('Tym razem przegrywasz :(');
+  }
+
+  if( argComputerMove == 'papier' && argPlayerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+  } else {
+    printMessage('Tym razem przegrywasz :(');
+  }
+
+  if( argComputerMove == 'nożyce' && argPlayerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+  } else {
+    printMessage('Tym razem przegrywasz :(');
+  }
+}
+
 let computerMove = getMoveName(randomNumber);
 /*
 if(randomNumber == 1){
@@ -17,16 +64,17 @@ if(randomNumber == 3){
 }
 
 printMessage('Mój ruch to: ' + computerMove);
-*/
+
 
 
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Gracz wpisał: ' + playerInput); */
 
-let playerMove - 'papier';
+let playerMove = = getMoveName(randomNumber);
 
+/*
 if(playerInput == '1'){
   playerMove = 'kamień';
 }
@@ -39,7 +87,7 @@ if(playerInput == '1'){
   playerMove = 'nożyce';
 }
 
-printMessage('Twój ruch to: ' + playerMove);
+printMessage('Twój ruch to: ' + playerMove); 
 
 
 
@@ -91,3 +139,4 @@ if( computerMove == 'papier' && playerMove == ''){
   printMessage('nieznany ruch!');
 }
 
+/*
